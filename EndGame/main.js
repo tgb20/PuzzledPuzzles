793b22d876f04322aeff2649053d5dd7061a03ad
Jacob Bashista <jacob@orange.haus>
2021-01-07 10:49:06 -0500

    Audio update

--- EndGame/main.js
+++ EndGame/main.js
@@ -1,50 +1,56 @@
 $(() => {
-    
+    $('#startButton').click(() => {
+        $('#audio').trigger('play');
+
+        $('#startButton').hide();
+        $('#ritual').show();
+        const ctrl = new anycontrol();
+
+        ctrl.addCommand("world", function () {
+            worldFound = true;
+            checkWords();
+        });
+
+        ctrl.addCommand("conjure", function () {
+            conjureFound = true;
+            checkWords();
+        });
+
+        ctrl.addCommand("trapped", function () {
+            trappedFound = true;
+            checkWords();
+        });
+
+        ctrl.addCommand("tonight", function () {
+            tonightFound = true;
+            checkWords();
+        });
+
+        ctrl.getCommand();
+        ctrl.start();
+    });
+
     let worldFound = false;
     let conjureFound = false;
     let trappedFound = false;
     let tonightFound = false;
 
-    const ctrl = new anycontrol();
 
-    ctrl.addCommand("world", function () {
-        worldFound = true;
-        checkWords();
-    });
-
-    ctrl.addCommand("conjure", function () {
-        conjureFound = true;
-        checkWords();
-    });
-
-    ctrl.addCommand("trapped", function () {
-        trappedFound = true;
-        checkWords();
-    });
-
-    ctrl.addCommand("tonight", function () {
-        tonightFound = true;
-        checkWords();
-    });
-
-    ctrl.start();
-
-    
     const checkWords = (() => {
-        if(worldFound) {
+        if (worldFound) {
             $('#world').show();
         }
-        if(conjureFound) {
+        if (conjureFound) {
             $('#conjure').show();
         }
-        if(trappedFound) {
+        if (trappedFound) {
             $('#trapped').show();
         }
-        if(tonightFound) {
+        if (tonightFound) {
             $('#tonight').show();
         }
 
-        if(worldFound && conjureFound && trappedFound && tonightFound) {
+        if (worldFound && conjureFound && trappedFound && tonightFound) {
             $('#gif').show();
         }
     });
