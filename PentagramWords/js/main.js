352ea7c4a402d58619601a459e9483eea705ee7d
tomerik-puzzled <tomerik@puzzledescapegames.com>
2021-01-13 16:59:36 -0500

    just missing linking to endgame

--- PentagramWords/js/main.js
+++ PentagramWords/js/main.js
@@ -6,10 +6,10 @@ function switchImages() {
 // first word: Spirits of our world and the next  = world
 // second word: lend us your sight = sight 
 // third word: give us the power to conjure our might  = conjure
-// fourth word: too long they've been trapped in the night  = trapped
+// fourth word: too long we've been  = trapped
 // fifth word: allow us to end our suffering tonight = suffering
 
-let words = [['', '', '', '', ''], ['', '', '', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', '', ''], ['', '', '', '', '', '', '']]
+let words = [['', '', '', '', ''], ['', '', '', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '']]
 
 $(() => {
 
@@ -19,11 +19,11 @@ $(() => {
         let letter = changedID[6];
         let word = changedID[11];
         words[word-1][letter-1] = $(this).val().toLowerCase();
-        let solution = words[0][2] + words[1][3] + words[2][1] + words[3][1] + words[4][2] + words[5][2];
-        // six letters. 
-        if(solution === 'butter') {
+        let solution = words[0][0] + words[1][0] + words[2][0] + words[3][0] + words[4][0] + words[4][8];
+        // six letters. wptcsu
+        if(solution === 'wptcsg') {
             console.log('Solved!');
-            $('#image').hide();
+            //$('#image').hide();
             $('#puzzle').hide();
             $('#video').show();
         }
@@ -35,11 +35,11 @@ $(() => {
         let letter = changedID[6];
         let word = changedID[11];
         words[word-1][letter-1] = $(this).val().toLowerCase();
-        let solution = words[0][2] + words[1][3] + words[2][1] + words[3][1] + words[4][2] + words[5][2];
+        let solution = words[0][0] + words[1][0] + words[2][0] + words[3][0] + words[4][0] + words[4][8];
         
-        if(solution === 'butter') {
+        if(solution === 'wptcsg') {
             console.log('Solved!');
-            $('#image').hide();
+            //$('#image').hide();
             $('#puzzle').hide();
             $('#video').show();
         }
