d53d6579180ad228d510e0023b6b5f7504ee5c24
Jacob Bashista <jacob@orange.haus>
2021-01-14 09:39:11 -0500

    EndGame

--- EndGame/main.js
+++ EndGame/main.js
@@ -1,18 +1,52 @@
 $(() => {
-    $('#startButton').click(() => {
-        $('#audio').trigger('play');
+    let worldFound = false;
+    let trappedFound = false;
+    let tonightFound = false;
 
-        $('#startButton').hide();
+    let chantsHeard = 0;
+
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if(!isChrome) {
+        $('#chromeMessage').show();
+        $('#scene').hide();
+    }
+
+    // Spirits of our WORLD and the next, lend us your SIGHT, 
+    // give us the power to CONJURE our might, 
+    // too long have they been trapped in the LIGHT, 
+    // allow us to end their suffering TONIGHT.
+
+    const checkWords = (() => {
+        if (worldFound && trappedFound && tonightFound) {
+            chantsHeard++;
+            worldFound = false;
+            trappedFound = false;
+            tonightFound = false;
+            
+            if(chantsHeard == 1) {
+                $('#allAudio').trigger('play');
+            }
+
+            if(chantsHeard == 3) {
+                $('#allAudio').trigger('pause');
+                $('#successAudio').trigger('play');
+                $('body').css('background-image', 'none');
+            }
+
+            $('#chantCounter').text(`Chants Heard: ${chantsHeard}`);
+
+        }
+    });
+
+    $('#startButton').click(() => {
+
+        $('#preZone').hide();
         $('#ritual').show();
         const ctrl = new anycontrol();
+        $('body').css('background-image', 'url("assets/h264.gif")');
 
         ctrl.addCommand("world", function () {
             worldFound = true;
-            checkWords();
-        });
-
-        ctrl.addCommand("conjure", function () {
-            conjureFound = true;
             checkWords();
         });
 
@@ -28,30 +62,5 @@ $(() => {
 
         ctrl.getCommand();
         ctrl.start();
-    });
-
-    let worldFound = false;
-    let conjureFound = false;
-    let trappedFound = false;
-    let tonightFound = false;
-
-
-    const checkWords = (() => {
-        if (worldFound) {
-            $('#world').show();
-        }
-        if (conjureFound) {
-            $('#conjure').show();
-        }
-        if (trappedFound) {
-            $('#trapped').show();
-        }
-        if (tonightFound) {
-            $('#tonight').show();
-        }
-
-        if (worldFound && conjureFound && trappedFound && tonightFound) {
-            $('#gif').show();
-        }
     });
 });
