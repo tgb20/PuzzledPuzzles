be13927780ab4168dc39f35ae81e060ca94f294a
tomerik-puzzled <tomerik@puzzledescapegames.com>
2021-03-30 22:31:27 -0400

    thingy

--- 1-CarSample/Puzzle3/script.js
+++ 1-CarSample/Puzzle3/script.js
@@ -1,13 +1,46 @@
+function myCountingFunctionX() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.klikkcountX) {
+      localStorage.klikkcountX = Number(localStorage.klikkcountX)+1;
+    } else {
+      localStorage.klikkcountX = 1;
+    }
+    document.getElementById("demo").innerHTML = "You have clicked the button " + localStorage.klikkcountX + " time(s).";
+  } else {
+    document.getElementById("demo").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
 
 
+function triggerPuzzleOrAudioX() {
 
-function featureAudio() {var x = document.getElementById("featureAudioId"); x.play();
-setTimeout(function() 
-		{document.getElementById("buttonId").style.display = "none";
+	if (localStorage.klikkcountX == undefined)
+	{
+	
+		var x = document.getElementById("featureAudioId"); x.play();
+		document.getElementById("buttonId").style.display = "block";
+		document.getElementById("squareId").style.display = "block";
+		//revealPuzzle();
+	}
+
+	else {
+		document.getElementById("buttonId").style.display = "none";
 		document.getElementById("squareId").style.display = "none";
+		var x = document.getElementById("featureAudioId"); x.pause();
+		document.getElementById("puzzle3rules").style.display = "block";
+		}
+	}  
 
-		}, 
-		100);}   
+
+
+
+function featureAudio() {
+		var x = document.getElementById("featureAudioId"); x.play();
+
+		document.getElementById("buttonId").style.display = "none";
+		document.getElementById("squareId").style.display = "none";
+		revealPuzzle();
+		}   
 
 
 
@@ -17,5 +50,5 @@ function revealPuzzle(){
                       document.getElementById("puzzle3rules").style.display = "block";
                       
                       
-                    }, 1 * 1000);
+                    }, 5 * 1000);
       }
