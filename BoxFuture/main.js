0ee77da7d077e5ce2593f0c9697949eb23355b3f
tomerik-puzzled <tomerik@puzzledescapegames.com>
2021-05-14 20:48:11 -0400

    psuh

--- BoxFuture/main.js
+++ BoxFuture/main.js
@@ -32,69 +32,8 @@ window.onload = (() => {
 
 
 
+ openIntroVideoFunction();
 
-
-
-
-
-// Get the Coordinates modal
-var coordinatesModal = document.getElementById("coordinatesModal");
-
-// Get the button that opens the modal
-var coordinatesBTN = document.getElementById("coordinatesButton");
-
-// Get the <span> element that closes the modal
-var coordinatesSpan = document.getElementsByClassName("closeCoordinates")[0];
-
-// When the user clicks on the button, open the modal
-coordinatesBTN.onclick = function() {
- coordinatesModal.style.display = "block";
- coordinatesBTN.style.display = "none";
-}
-
-// When the user clicks on <span> (x), close the modal
-coordinatesSpan.onclick = function() {
-  coordinatesModal.style.display = "none";
-  coordinatesBTN.style.display = "block";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == coordinatesModal) {
-    coordinatesModal.style.display = "none";
-    coordinatesBTN.style.display = "block";
-  }
-}
-
-
-
-
-  /*
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-//var btn = document.getElementById("padlockButton");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-//btn.onclick = function() {
-//  modal.style.display = "block";
-//}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 
 
